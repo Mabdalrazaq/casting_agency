709bf3b9b484166a3b1dcf2b8a92eecf608bf76b
mabdalrazaq <m.abdalrazaq314@gmail.com>
2021-01-27 09:25:20 +0200

    edit11

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh setup.sh && gunicorn app:app
+web: gunicorn app:app
--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
-export AUTH0_DOMAIN = 'moapps.us.auth0.com'
-export ALGORITHMS = ['RS256']
-export API_AUDIENCE = 'casting_agency'
+AUTH0_DOMAIN='moapps.us.auth0.com'
+ALGORITHMS=['RS256']
+API_AUDIENCE='casting_agency'
+DATABASE_URL='postgres://tilbbxzseahede:bee1423d6be6124f596b746ebe4d2ff497d013e29ee849ac5077083e3227f435@ec2-23-20-205-19.compute-1.amazonaws.com:5432/df9b4d8897hc33'
+HEROKU_POSTGRESQL_MAUVE_URL='postgres://nwlkvmtkdguwiv:60596e6b50a64a0a6946f12f9a2fa9beea9656cb8b625a2e4efe40511b407bf4@ec2-3-216-181-219.compute-1.amazonaws.com:5432/dbo4i0pv0nfdc0'
